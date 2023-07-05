fcd6de6254b383a9dcfdeb7bcad6d31c5b3ce64f
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-07-06 04:24:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from typing import Dict
 
 from config import settings
 
-app = FastAPI(dependencies=[Depends(RateLimiter(times=1, seconds=5))])
+app = FastAPI(dependencies=[Depends(RateLimiter(times=1, seconds=80))])
 logger = logging.getLogger(__name__)
 # .env variables can be validated and accessed from the config, here to set a log level
 logging.basicConfig(level=settings.LOG_LEVEL.upper())
